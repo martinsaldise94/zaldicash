5448bdaf9da7637efe5251b70872a387527e0856
martinsaldise94 <martinsaldise94@gmail.com>
2026-03-27 09:45:27 +0100

    configuración de sequelize

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 Proyecto de gestión de finanzas personales | React, Express &amp; SQL | Proyecto de Zaldicode
 
 Actualmente en desarrollo.
+
+Aplicación de seguimiento de finanzas personales en una sola app.
+Es común en las inversiones tener activos en distintas plataformas. Con Zaldicash, se unificarán todas en una misma aplicación y se podrá seguir de manera REAL la evolución de nuestras inversiones.
---
+++ server/config/config.js
@@ -0,0 +1,23 @@
+require("dotenv").config({ path: "../.env" }); // Buscamos el .env que está en la raíz de /server
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_DATABASE || "zaldicash_db",
+    host: process.env.DB_HOST || "127.0.0.1",
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres",
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: "zaldicash_test",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  production: {
+    use_env_variable: "DATABASE_URL", // Para cuando suba a internet todo
+    dialect: "postgres",
+  },
+};
